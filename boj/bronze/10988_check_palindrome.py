0df740ca1d4a89d44e99fb3c83215c74794c483d
ilwoo <53050413+Oilwoo@users.noreply.github.com>
2024-12-25 23:41:00 +0900

    12.25 보충

--- boj/bronze/10988_check_palindrome.py
+++ boj/bronze/10988_check_palindrome.py
@@ -5,3 +5,8 @@ if word == word_reverse:
     print(1)
 else:
     print(0)
+    
+# reverse는 list타입에서 제공하는 함수이다.
+# reverse는 값을 반환하지 않고, 단순히 해당 list를 뒤섞어준다.
+# reversed는 내장함수로, list에서 제공하는 함수가 아니다.
+# reversed는 ‘reversed’ 객체를 반환한다.
